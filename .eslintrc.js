70796770f745a8798c95cac4d21cfdf52809c1ff
Ferhat Attar <attarferhat@hotmail.fr>
2019-05-07 22:54:50 +0200

    Setup project

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true,
+        "jest/globals": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:jest/recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": ["jest"],
+    "rules": {
+        "no-console": 0,
+        "jest/no-disabled-tests": "warn",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "error"
+    }
+};
